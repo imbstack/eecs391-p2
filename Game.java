8a20448af82830ee4c0ecc77f003d702c0dc571d
Brian Stack <bis12@case.edu>
2011-02-21 23:11:12 -0500

    Testing against old commit

--- Game.java
+++ Game.java
@@ -1,13 +1,16 @@
 import java.awt.Point;
 import java.awt.Dimension;
+import java.util.LinkedList;
 
 class Game{
 	private Dimension gridDims;
 	private short[][] grid;
+	private boolean[][] explored;
 
 	public Game(int width, int height, Point... mines){
 		gridDims = new Dimension(width, height);
 		grid = new short[gridDims.width][gridDims.height];
+		explored = new boolean[gridDims.width][gridDims.height];
 		for(Point mine : mines){
 			grid[mine.x][mine.y] = 9;
 		}
@@ -16,6 +19,7 @@ class Game{
 				grid[i][j] = countAdjacentMines(i,j);
 			}
 		}
+		//select(0,0);
 	}
 
 	public Game(){
@@ -27,11 +31,38 @@ class Game{
 	 * of mines, and settable grid size, just like the real game.
 	 */
 
+
+	public boolean select(int x, int y){
+		explored[x][y] = true;
+		if(grid[x][y] == 9){
+			System.out.println("-----Game Over-----");
+			return false;
+		}
+		else if (grid[x][y] == 0){
+			for (Point p : getAdjacentMines(x,y)){
+			if(grid[p.x][p.y] == 0){
+				select(p.x,p.y);
+			}	
+		}
+		}
+		return true;
+	}
+
 	private short countAdjacentMines(int x, int y){
 		if (grid[x][y] == 9){
 			return 9;
 		}
 		short mineCount = 0;
+		for (Point p : getAdjacentMines(x,y)){
+			if(grid[p.x][p.y] == 9){
+				mineCount++;
+			}	
+		}
+		return mineCount;
+	}
+
+	private LinkedList<Point> getAdjacentMines(int x, int y){
+		LinkedList<Point> adjacents = new LinkedList<Point>();
 		int cx,cy;
 		for(int i = -1; i <= 1; i++){
 			for(int j = -1; j <= 1; j++){
@@ -40,23 +71,21 @@ class Game{
 				if(cx > 0 && cx < gridDims.width && 
 						cy > 0 && cy < gridDims.height &&
 						x != 0 && y != 0){
-					if(grid[cx][cy] == 9){
-						mineCount++;
-					}
+					adjacents.add(new Point(cx, cy));
 						}	
 
 			}	
 		}
-		return mineCount;
+		return adjacents;
 	}
-	
+
 	//Printing methods --------------//
 	//Reverse the x and y directions to 
 	//make this agree with the outside world
 	//-------------------------------//
 
 	public void printFullGrid(){
-		System.out.println("=Values====================");
+		System.out.println("-Values====================");
 		for(int i = 0; i < gridDims.width; i++){
 			for(int j = 0; j < gridDims.height; j++){
 				System.out.print(grid[j][i] + " ");
@@ -65,9 +94,9 @@ class Game{
 		}
 		System.out.println("===========================");
 	}
-	
+
 	public void showMines(){
-		System.out.println("=Mines=====================");
+		System.out.println("-Mines=====================");
 		for(int i = 0; i < gridDims.width; i++){
 			for(int j = 0; j < gridDims.height; j++){
 				if(grid[j][i] == 9){
@@ -75,6 +104,30 @@ class Game{
 				}
 				else{
 					System.out.print("◻ ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("===========================");
+	}
+
+	public void printState(){
+		System.out.println("-Move======================");
+		for(int i = 0; i < gridDims.width; i++){
+			for(int j = 0; j < gridDims.height; j++){
+				if(explored[j][i]){
+					if(grid[j][i] == 9){
+						System.out.print("✸ ");
+					}
+					else if(grid[j][i] == 0){
+						System.out.print("◻ ");
+					}
+					else{
+						System.out.print(grid[i][j] + " ");
+					}
+				}
+				else{
+					System.out.print("◼ ");
 				}
 			}
 			System.out.println();
--- Minesweeper.java
+++ Minesweeper.java
@@ -2,6 +2,7 @@ class Minesweeper{
 	public static void main(String[] args){
 		Game play = new Game();
 		play.printFullGrid();
-		play.showMines();
+		//play.showMines();
+		play.printState();
 	}
 }
